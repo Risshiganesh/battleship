d6d49ebe0f0bfa0e6d617e6680bc0ee3a2e8fecf
Risshiganesh <risshiganesh1997@gmail.com>
2024-04-17 09:24:36 +0800

    getCoordinates function complete

--- src/gameboardFactory.js
+++ src/gameboardFactory.js
@@ -3,29 +3,119 @@ function createGameboard(){
 
     function createOwnGrid () {
 
-        const columns = []
+        const rows = []
 
-        columns.length = 10;
+        rows.length = 10;
 
         const gridSize = 10;
 
         for (let index = 0; index < gridSize - 1; index++) {
-            ownGrid[index] = columns
+            ownGrid[index] = rows
         }
 
-        ownGrid.push(columns)
+        ownGrid.push(rows)
 
         // should each grid have an object with coords? x and y?
+    }
+
+
+
+    function placeShip (coordinates, direction, shipObject) {
+
+
+        const potentialCoords = getCoordinates(coordinates, direction, shipObject)
+
+        
+        if (!potentialCoords.validity) {
+            return
+        }
+
+        potentialCoords.potentialCoordinates.forEach(coordinates => {
+            ownGrid[coordinates[0]][coordinates[1]] = shipObject
+        });
+
+       
+        
     }
     
     createOwnGrid()
 
     return {
-        ownGrid
+        ownGrid,
+        placeShip
     }
+
+
 
 }
 
+
+function getCoordinates (coordinates, direction, shipObject) {
+
+    let validity = true;
+
+    const potentialCoordinates = []
+
+    
+    if (coordinates.x < 0 || coordinates.y < 0 || coordinates.x > 9 || coordinates.y > 9 ) {
+            
+        validity = false
+
+        return validity
+
+    }
+
+    for (let index = 0; index < shipObject.getLength(); index++) {
+
+        
+        let xcoords;
+        let ycoords;
+
+        if (direction === "north") {
+            xcoords = coordinates.x
+            ycoords = coordinates.y - index;
+
+            
+        }
+        if (direction === "south") {
+
+            xcoords = coordinates.x
+            ycoords = coordinates.y + index;
+
+        }
+        if (direction === "east") {
+
+            xcoords = coordinates.x + index;
+            ycoords = coordinates.y; 
+
+            
+        }
+        if (direction === "west") {
+            xcoords = coordinates.x - index;
+            ycoords = coordinates.y; 
+
+        }
+
+        if (xcoords < 0 || ycoords < 0 || xcoords > 9 || ycoords > 9 ) {
+            
+            validity = false
+
+        }
+
+        potentialCoordinates.push([xcoords,ycoords])
+        
+    }
+
+
+    return {
+    
+        validity,
+        potentialCoordinates
+    }
+}
+
+
 export{
-    createGameboard
+    createGameboard,
+    getCoordinates
 }
--- src/gameboardFactory.test.js
+++ src/gameboardFactory.test.js
@@ -1,4 +1,5 @@
-import {createGameboard} from './gameboardFactory';
+import {createGameboard, getCoordinates} from './gameboardFactory';
+import { createShip } from './shipFactory';
 
 describe('Test gameboard factory',function () {
     const gameboard = createGameboard()
@@ -8,36 +9,545 @@ describe('Test gameboard factory',function () {
         expect(Array.isArray(gameboard.ownGrid)).toBe(true)
     });
 
-    test('Gameboard row size: 10',function () {
+    test('Gameboard column size: 10',function () {
         
         expect(gameboard.ownGrid.length).toBe(10);
         
     });
 
-    test("Gameboard column size: 10", function () {
+    test("Gameboard row size: 10", function () {
         
-        let tenColumns = true
-        const rows = gameboard.ownGrid;
+        let tenRows = true
+        const columns = gameboard.ownGrid;
 
 
-        for (let index = 0; index < rows.length; index++) {
-            const row = rows[index];
-            // console.debug("row", row)
-            if (row.length !== 10) {
-                tenColumns = false;
+        for (let index = 0; index < columns.length; index++) {
+            const column = columns[index];
+
+            if (column.length !== 10) {
+                tenRows = false;
                 break;
             }
 
         }
 
-        expect(tenColumns).toBe(true);
+        expect(tenRows).toBe(true);
 
     })
 
-    test("Call placeShip() function with ship factory function", function () {
-        expect(gameboard.placeShip(x,y,`mock function???? createShip()`)).toBeCalledWith();
-        // update tests and code to make sure it creates column first and the rows, now it's the other way round.
+    
+
+    
+
+
+    
+
+})
+
+// Test this method with ships facing at different directions and different ship size.
+
+describe("Test gameboard's placeShip method", function () {
+
+    
+
+    describe("Test placeShip() if direction is north", function() {
+
+        test("North with coordinates = x: 0, y: 0", function () {
+
+            const gameboard = createGameboard()
+            
+            const coordinates = {
+                x: 0,
+                y: 0
+            }
+    
+            const direction = "north";
+    
+            const shipObject = createShip(3)
+    
+            
+    
+            gameboard.placeShip(coordinates,direction,shipObject)
+    
+    
+    
+            expect(gameboard.ownGrid[coordinates.x][coordinates.y]).toEqual(undefined)
+    
+            // This test is failing, create the grid number filter (Do not assign object to grid if not valid coordinates).
+        });
+
+        test("North with coordinates = x: 9, y: 9", function () {
+
+            const gameboard = createGameboard()
+            
+            const coordinates = {
+                x: 9,
+                y: 9
+            }
+    
+            const direction = "north";
+    
+            const shipObject = createShip(3)
+    
+            
+    
+            gameboard.placeShip(coordinates,direction,shipObject)
+    
+    
+    
+            expect(gameboard.ownGrid[coordinates.x][coordinates.y]).toEqual(shipObject)
+    
+        });
+
     })
+
+    
+
+    test("Test placeShip() if direction is south", function () {
+
+        const gameboard = createGameboard()
+        
+        const coordinates = {
+            x: 0,
+            y: 0
+        }
+
+        const direction = "south";
+
+        const shipObject = createShip(3);
+
+        gameboard.placeShip(coordinates,direction,shipObject)
+
+        expect(gameboard.ownGrid[coordinates.x][coordinates.y]).toEqual(shipObject)
+        expect(gameboard.ownGrid[coordinates.x][1]).toEqual(shipObject)
+        expect(gameboard.ownGrid[coordinates.x][2]).toEqual(shipObject)
+        expect(gameboard.ownGrid[coordinates.x][3]).toEqual(undefined)
+
+
+       
+
+    });
+
+
+    test("Test placeShip() if direction is east", function () {
+
+        const gameboard = createGameboard()
+        
+        const coordinates = {
+            x: 0,
+            y: 0
+        }
+
+        const direction = "east";
+
+        const shipObject = createShip(3);
+
+        gameboard.placeShip(coordinates,direction,shipObject)
+
+        expect(gameboard.ownGrid[coordinates.x][coordinates.y]).toEqual(shipObject)
+        expect(gameboard.ownGrid[1][coordinates.y]).toEqual(shipObject)
+        expect(gameboard.ownGrid[2][coordinates.y]).toEqual(shipObject)
+        expect(gameboard.ownGrid[3][coordinates.y]).toEqual(shipObject)
+
+
+       
+
+    });
+
+
+    test("Test placeShip() if direction is west", function () {
+
+        const gameboard = createGameboard()
+        
+        const coordinates = {
+            x: 0,
+            y: 0
+        }
+
+        const direction = "west";
+
+        const shipObject = createShip(3);
+
+        gameboard.placeShip(coordinates,direction,shipObject)
+
+        expect(gameboard.ownGrid[coordinates.x][coordinates.y]).toEqual(undefined)
+
+
+
+       
+
+    })
+})
+
+
+// Put this in its own describe block later
+
+test.skip("Test receiveAttack method", function () {
+        
+    expect().toBe()
+
+})
+
+
+describe("Test getCoordinates function", function () {
+
+    describe("Test if direction is north", function () {
+
+        const direction = "north";
+    
+        const shipObject = createShip(3);
+
+        const expectedValidity = {
+            x0y0:false,
+            x9y9:true,
+            x0y9:true,
+            x9y0:false,
+            x5y5:true
+
+        }
+        const expectedCoords = {
+            coords1: [[0,0],[0,-1],[0,-2]],
+            coords2: [[9,9],[9,8],[9,7]],
+            coords3: [[0,9],[0,8],[0,7]],
+            coords4: [[9,0],[9,-1],[9,-2]],
+            coords5: [[5,5],[5,4],[5,3]]
+        }
+
+        test("Coordinates = x:0, y:0", function () {
+
+            const coordinates = {
+                x: 0,
+                y: 0
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y0);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords1)
+        })
+    
+        test("Coordinates = x:9, y:9", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords2)
+        })
+    
+    
+        test("Coordinates = x:0, y:9", function () {
+    
+            const coordinates = {
+                x: 0,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords3)
+        })
+    
+        test("Coordinates = x:9, y:0", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 0
+            }
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y0)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords4)
+        })
+    
+    
+        test("Coordinates = x:5, y:5", function () {
+    
+            const coordinates = {
+                x: 5,
+                y: 5
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x5y5)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords5)
+        })
+    
+    
+        
+    })
+
+
+    describe("Test if direction is south", function () {
+
+        const direction = "south";
+    
+        const shipObject = createShip(3);
+
+        const expectedValidity = {
+            x0y0:true,
+            x9y9:false,
+            x0y9:false,
+            x9y0:true,
+            x5y5:true
+
+        }
+
+        const expectedCoords = {
+            coords1: [[0,0],[0,1],[0,2]],
+            coords2: [[9,9],[9,10],[9,11]],
+            coords3: [[0,9],[0,10],[0,11]],
+            coords4: [[9,0],[9,1],[9,2]],
+            coords5: [[5,5],[5,6],[5,7]]
+        }
+
+        test("Coordinates = x:0, y:0", function () {
+
+            const coordinates = {
+                x: 0,
+                y: 0
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y0);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords1)
+        })
+    
+        test("Coordinates = x:9, y:9", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords2)
+        })
+    
+    
+        test("Coordinates = x:0, y:9", function () {
+    
+            const coordinates = {
+                x: 0,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords3)
+        })
+    
+        test("Coordinates = x:9, y:0", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 0
+            }
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y0);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords4)
+        })
+    
+    
+        test("Coordinates = x:5, y:5", function () {
+    
+            const coordinates = {
+                x: 5,
+                y: 5
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x5y5);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords5)
+        })
+    
+    
+        
+    })
+
+
+
+    describe("Test if direction is east", function () {
+
+        const direction = "east";
+    
+        const shipObject = createShip(3);
+
+        const expectedValidity = {
+            x0y0:true,
+            x9y9:false,
+            x0y9:true,
+            x9y0:false,
+            x5y5:true
+
+        }
+
+        const expectedCoords = {
+            coords1: [[0,0],[1,0],[2,0]],
+            coords2: [[9,9],[10,9],[11,9]],
+            coords3: [[0,9],[1,9],[2,9]],
+            coords4: [[9,0],[10,0],[11,0]],
+            coords5: [[5,5],[6,5],[7,5]]
+        }
+
+        test("Coordinates = x:0, y:0", function () {
+
+            const coordinates = {
+                x: 0,
+                y: 0
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y0);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords1)
+        })
+    
+        test("Coordinates = x:9, y:9", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords2)
+        })
+    
+    
+        test("Coordinates = x:0, y:9", function () {
+    
+            const coordinates = {
+                x: 0,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords3)
+        })
+    
+        test("Coordinates = x:9, y:0", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 0
+            }
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y0)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords4)
+        })
+    
+    
+        test("Coordinates = x:5, y:5", function () {
+    
+            const coordinates = {
+                x: 5,
+                y: 5
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x5y5)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords5)
+        })
+    
+    
+        
+    })
+
+
+
+    describe("Test if direction is west", function () {
+
+        const direction = "west";
+    
+        const shipObject = createShip(3);
+
+        const expectedValidity = {
+            x0y0:false,
+            x9y9:true,
+            x0y9:false,
+            x9y0:true,
+            x5y5:true
+
+        }
+
+        const expectedCoords = {
+            coords1: [[0,0],[-1,0],[-2,0]],
+            coords2: [[9,9],[8,9],[7,9]],
+            coords3: [[0,9],[-1,9],[-2,9]],
+            coords4: [[9,0],[8,0],[7,0]],
+            coords5: [[5,5],[4,5],[3,5]]
+        }
+
+        test("Coordinates = x:0, y:0", function () {
+
+            const coordinates = {
+                x: 0,
+                y: 0
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y0);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords1)
+        })
+    
+        test("Coordinates = x:9, y:9", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y9);
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords2)
+        })
+    
+    
+        test("Coordinates = x:0, y:9", function () {
+    
+            const coordinates = {
+                x: 0,
+                y: 9
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x0y9)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords3)
+        })
+    
+        test("Coordinates = x:9, y:0", function () {
+    
+            const coordinates = {
+                x: 9,
+                y: 0
+            }
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x9y0)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords4)
+        })
+    
+    
+        test("Coordinates = x:5, y:5", function () {
+    
+            const coordinates = {
+                x: 5,
+                y: 5
+            }
+    
+            
+            expect(getCoordinates(coordinates,direction,shipObject).validity).toBe(expectedValidity.x5y5)
+            expect(getCoordinates(coordinates,direction,shipObject).potentialCoordinates).toEqual(expectedCoords.coords5)
+        })
+    
+    
+        
+    })
+    
+    
 
 
 })
