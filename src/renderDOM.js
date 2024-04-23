e221d8adbfd7cd0a6145277917a368a7c57478a1
Risshiganesh <risshiganesh1997@gmail.com>
2024-04-23 16:37:17 +0800

    Boards frame CSS done

--- src/renderDOM.js
+++ src/renderDOM.js
@@ -27,12 +27,33 @@ function createDOM () {
     const player1Div = document.createElement('div');
     player1Div.classList.add('player1-div');
     playerDiv.append(player1Div);
-    player1Div.textContent = "Player 1";
+    // player1Div.textContent = "Player 1";
+
+        const player1Grid = document.createElement('div');
+        player1Grid.classList.add('player1-grid');
+        player1Div.append(player1Grid);
+
+        const player1Label = document.createElement('div');
+        player1Label.classList.add('player1-label');
+        player1Label.textContent = "Player 1"
+        player1Div.append(player1Label)
 
     const player2Div = document.createElement('div');
     player2Div.classList.add('player2-div');
     playerDiv.append(player2Div)
-    player2Div.textContent = "Player 2";
+    // player2Div.textContent = "Player 2";
+
+        const player2Grid = document.createElement('div');
+        player2Grid.classList.add('player2-grid');
+        player2Div.append(player2Grid);
+
+        const player2Label = document.createElement('div');
+        player2Label.classList.add('player2-label');
+        player2Label.textContent = "Player 2"
+        player2Div.append(player2Label)
+
+
+    // Create board div, make sure there is minimum size for height and width
 }
 
 export {
